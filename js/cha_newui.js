aff584453ba5015b01be73134faca8c42ebabb20
mkzi_nya <wcnmngsb999@gmail.com>
2025-03-14 11:09:47 +0800

    Add files via upload

--- js/cha_newui.js
+++ js/cha_newui.js
@@ -194,7 +194,7 @@ const constants = {
   "ca751a94-f71e-4556-9d54-76de325ca29c": { constant: 2.0, category: "DZ", name: "Moonfall／大月墜落狂想", ad: 0, ae: 1, af: 1, ag: 0 },
 "addba2e9-ad45-4770-b850-717ab2e0e17d": { constant: 10.6, category: "CB", name: "FULi AUTO SHOOTER", ad: 0, ae: 1, af: 1, ag: 0 },
 "423e7ae7-97e9-4c87-a778-36af2020a59d": { constant: 7.2, category: "SK", name: "FULi AUTO SHOOTER", ad: 0, ae: 1, af: 1, ag: 0 },
-"54c60c83-9a80-458d-bf11-b04be1af1977": { constant: 3.0, category: "DZ", name: "FULi AUTO SHOOTER", ad: 0, ae: 1, af: 1, ag: 0 },
+"d93cfe14-d3b1-4934-98a4-77b89e1f1304": { constant: 3.0, category: "DZ", name: "FULi AUTO SHOOTER", ad: 0, ae: 1, af: 1, ag: 0 },
 "825405cb-d262-4f10-9e98-4d967e4e33f4": { constant: 11.3, category: "CB", name: "cafe in 6412l731V", ad: 0, ae: 1, af: 1, ag: 0 },
 "faf0dc82-df37-456a-b726-283e27df9c31": { constant: 7.0, category: "SK", name: "cafe in 6412l731V", ad: 0, ae: 1, af: 1, ag: 0 },
 "54c60c83-9a80-458d-bf11-b04be1af1977": { constant: 2.0, category: "DZ", name: "cafe in 6412l731V", ad: 0, ae: 1, af: 1, ag: 0 },
