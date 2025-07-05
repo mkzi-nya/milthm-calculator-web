5193b0dbdc16c7fbc4a3bd3be32e08c4896e7bd0
lmsdscav <lmsdscavmc@hotmail.com>
2025-07-05 12:53:34 +0800

    meow

--- push.py
+++ push.py
@@ -1,15 +1,12 @@
-import random
 import os
 import sys
-
-def rdBytes(length):
-    return "".join([random.choice("abcdef1234567890") for _ in range(length)]).encode()
-
+import hashlib
 def x(ouo):
     if not ouo in ['y','Y','yes','Yes','YES']:
         print("abort.")
         exit(0)
-
+def short(c):
+    return c if len(c)<=16 else c[:15]
 # 自动切换目录
 orig_dir = os.getcwd()
 script_dir = os.path.abspath(os.path.dirname(__file__))
@@ -17,8 +14,6 @@ os.chdir(script_dir)
 
 # 判断是否带y参数
 auto_yes = len(sys.argv) > 1 and sys.argv[1].lower() == 'y'
-
-rn = rdBytes(16)
 
 if not auto_yes:
     ui = input("真的要继续吗？确保你的修改已应用于index.model.html，所有index.html的修改将丢失！(y/[n])：")
@@ -28,7 +23,7 @@ with open("index.model.html", "rb") as f:
     c = f.read()
 
 with open("index.html", "wb+") as f:
-    r = c.replace(b'!##oiiaiooiiiai##!', rn)
+    r = c.replace(b'!##oiiaiooiiiai##!', short(hashlib.md5(c).hexdigest()).encode())
     f.write(r)
 
 cmd = "git add . && git commit -m \"meow\" && git push origin main"
